a8b8103bc901e2b69db1f6fa1581786df257f3f8
onifernando1 <onifernando1@gmail.com>
2022-09-15 17:03:46 +0100

    invite crud

--- app/controllers/invites_controller.rb
+++ app/controllers/invites_controller.rb
@@ -1,4 +1,25 @@
 class InvitesController < ApplicationController
 
-    
+    def index 
+        @invites = Invite.all
+    end 
+
+    def show
+        @invite = Invite.find(param[:id]) 
+    end 
+
+    def new
+        @invite = Invite.new 
+    end 
+
+    def create 
+        @invite = current_user.invite.build()
+        if @invite.save
+            redirect_to @invite
+        else 
+            render :new, status: :unprocessable_entity
+        end 
+    end 
+
+
 end
