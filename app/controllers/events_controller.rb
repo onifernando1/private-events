3206969ee4285d5dd99b3194dabeeb37c86816b8
onifernando1 <onifernando1@gmail.com>
2022-09-15 15:21:27 +0100

    add user authentication

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,7 @@
 class EventsController < ApplicationController
 
+    before_action :authenticate_user!
+
     def index
         @events = Event.all() 
     end 
@@ -14,6 +16,7 @@ class EventsController < ApplicationController
 
     def create
         @event= Event.new(event_params)
+        @event.build_user(current_user)
         if @event.save
             redirect_to @event
         else 
