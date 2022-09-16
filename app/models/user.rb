18f86a16433c2949a5ad457389d4d2e8944e66bf
onifernando1 <onifernando1@gmail.com>
2022-09-16 14:00:30 +0100

    allow names params

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :events, foreign_key: "creator_id"
   has_many :invites, foreign_key: :attendee_id
-  has_many :attended_events, through: :invites, source: :event
+  has_many :attended_events, through: :invites
   # source: :event
 end
