56f29844e80ba5da3418e5a307bd1e0bed7f0c8b
onifernando1 <onifernando1@gmail.com>
2022-09-16 13:22:02 +0100

    fix associations

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,6 @@
 class Event < ApplicationRecord
     belongs_to :creator, foreign_key:"creator_id", class_name: "User"
     has_many :invites, foreign_key: :attended_event_id
-    has_many :attendees, through: :invites, source: :user
+    has_many :attendees, through: :invites
 
 end
