5136ee5518fe0aa51cf6d218d6984336adc67542
onifernando1 <onifernando1@gmail.com>
2022-09-17 17:12:06 +0100

    Date logic

--- app/models/event.rb
+++ app/models/event.rb
@@ -8,7 +8,7 @@ class Event < ApplicationRecord
     validates :date, presence: true  
     validates :location, presence: true  
 
-    scope :past, -> {where("date: < ?", Date.today)}
-    scope :future, -> {where("date: > ?", Date.today)}
+    scope :past, -> {where("date < ?", Date.today)}
+    scope :future, -> {where("date > ?", Date.today)}
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_16_122211) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_17_130936) do
   create_table "events", force: :cascade do |t|
     t.string "title"
     t.string "description"
-    t.datetime "date"
+    t.date "date"
     t.string "location"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
